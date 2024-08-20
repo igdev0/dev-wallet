454c3f56f99173ba7ba8825a0b9e99390bc5a9a7
igdev <dorultanianos@gmail.com>
2024-08-20 18:10:13 +0300

    refactor: app state

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,7 +1,10 @@
-use sqlx::{Connection, SqliteConnection};
+pub use sqlx::{Connection, SqliteConnection};
+
+struct Ab {
+    db: SqliteConnection,
+}
 
 pub async fn create_db_connection() -> Result<SqliteConnection, sqlx::Error> {
-    let conn = SqliteConnection::connect("sqlite::memory:").await?;
-
+    let conn = SqliteConnection::connect("sqlite://database.db").await?;
     Ok(conn)
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::sync::Mutex;
+use bitcoin_wallet::db;
+use sqlx::SqliteConnection;
+use std::sync::{Arc, Mutex};
 use tauri::{Manager, State};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
-#[derive(Default)]
 struct AppState {
-    mnemonics: String,
+    mnemonics: Arc<Mutex<String>>,
+    db_pool: Arc<Mutex<SqliteConnection>>,
 }
 
 #[tauri::command]
-fn generate_mnemonic(state: State<'_, Mutex<AppState>>) -> String {
+fn generate_mnemonic(state: State<'_, AppState>) -> String {
     let mnemonics: String = bitcoin_wallet::utils::generate_mnemonic()
         .unwrap()
         .to_string();
-    let mut state = state.lock().unwrap();
-    state.mnemonics = mnemonics;
-    state.mnemonics.clone()
+
+    let mut m = state.mnemonics.lock().unwrap();
+    *m = mnemonics.clone();
+
+    mnemonics
 }
 
 #[async_std::main]
 async fn main() {
+    let db_pool = db::create_db_connection().await.unwrap();
+    let app_state = AppState {
+        mnemonics: Arc::new(Mutex::new("".to_string())),
+        db_pool: Arc::new(Mutex::new(db_pool)),
+    };
+
     tauri::Builder::default()
         .setup(|app| {
-            app.manage(Mutex::new(AppState::default()));
+            app.manage(app_state);
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![generate_mnemonic])
