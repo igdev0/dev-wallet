c814db135aa86f5db2c53e1c09741291796ee646
igdev <dorultanianos@gmail.com>
2024-09-04 13:38:56 +0300

    feat(accounts): add new screen

---
+++ src/screens/accounts.tsx
@@ -0,0 +1,10 @@
+import Screen from "../components/screen.tsx";
+import { Heading } from "@chakra-ui/react";
+
+export default function Accounts() {
+  return (
+    <Screen>
+      <Heading>Accounts screen</Heading>
+    </Screen>
+  );
+}
