e437a38f97a663b4b13068d4685c0e63922bd502
igdev <dorultanianos@gmail.com>
2024-09-08 10:07:00 +0300

    feat(migration): add network and blockchain to account table

--- src-tauri/migrations/20240809_create_account_table.sql
+++ src-tauri/migrations/20240809_create_account_table.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS accounts (
     address NOT NULL UNIQUE,
     "index" INTEGER NOT NULL,
     path TEXT NOT NULL UNIQUE,
+    network TEXT NOT NULL,
+    blockchain TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
