d8c87f186b56ac53767965333ebe0c01b1868a34
igdev <dorultanianos@gmail.com>
2024-09-08 10:08:38 +0300

    chore(create_account): remove index column

--- src-tauri/migrations/20240809_create_account_table.sql
+++ src-tauri/migrations/20240809_create_account_table.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS accounts (
     id UUID PRIMARY KEY,
     wallet_id UUID REFERENCES wallets(id),
     address NOT NULL UNIQUE,
-    "index" INTEGER NOT NULL,
     path TEXT NOT NULL UNIQUE,
     network TEXT NOT NULL,
     blockchain TEXT NOT NULL,
