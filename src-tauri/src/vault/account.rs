0218ea438adb417649f12a238c9a645b677d5ae2
igdev <dorultanianos@gmail.com>
2024-09-09 11:33:47 +0300

    feat(account): impl account builder functionalities

--- src-tauri/src/vault/account.rs
+++ src-tauri/src/vault/account.rs
@@ -1,8 +1,27 @@
-use core::fmt;
+use core::{fmt, slice::SlicePattern};
+
+use crate::utils::{decrypt, encrypt, AESKey};
+use bitcoin::{
+    bip32::{DerivationPath, Xpriv},
+    hex::DisplayHex,
+    secp256k1, Address, CompressedPublicKey, Network as BitcoinNetwork, NetworkKind, PrivateKey,
+    PubkeyHash,
+};
+use thiserror::Error;
 
 const BITCOIN: &str = "Bitcoin";
 const TESTNET: &str = "Testnet";
 const MAINNET: &str = "Mainnet";
+
+#[derive(Debug, Error)]
+enum AccountError {
+    #[error("Failed building")]
+    Building,
+    #[error("Path invalid")]
+    Path,
+    #[error("Failed deriving key form path")]
+    Derivation,
+}
 
 #[derive(Default, Debug)]
 pub struct AccountModel {
@@ -15,7 +34,7 @@ pub struct AccountModel {
     pub created_at: Option<String>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 
 pub enum Blockchain {
     #[default]
@@ -36,6 +55,12 @@ impl Blockchain {
         match text {
             BITCOIN => Ok(Blockchain::Bitcoin),
             _ => Err("Error parsing"),
+        }
+    }
+
+    pub fn to_bitcoin_network(&self) -> BitcoinNetwork {
+        match self {
+            Blockchain::Bitcoin => BitcoinNetwork::Bitcoin,
         }
     }
 }
@@ -65,6 +90,13 @@ impl Network {
             _ => Err("Error parsing"),
         }
     }
+
+    pub fn to_bitcoin_network_kind(&self) -> NetworkKind {
+        match self {
+            Network::Mainnet => NetworkKind::Main,
+            Network::Testnet => NetworkKind::Test,
+        }
+    }
 }
 
 impl From<StoreAccountInput> for AccountModel {
@@ -83,20 +115,87 @@ impl From<StoreAccountInput> for AccountModel {
 
 #[derive(Default, Debug)]
 pub struct StoreAccountInput {
-    wallet_id: String,
-    address: String,
-    encrypted_path: String,
-    blockchain: Blockchain,
-    network: Network,
+    pub wallet_id: String,
+    pub address: String,
+    pub encrypted_path: String,
+    pub blockchain: Blockchain,
+    pub network: Network,
 }
+
+type AccountInputBuilderResult = Result<StoreAccountInput, AccountError>;
 
 #[derive(Default, Debug)]
 pub struct AccountInputBuilder {
-    path: String,
-    blockchain: Blockchain,
-    network: Network,
+    pub path: DerivationPath,
+    pub blockchain: Blockchain,
+    pub network: Network,
+    pub encrypted_seed: &'static str,
+    pub wallet_id: String,
+}
+
+impl From<DerivationPath> for AccountInputBuilder {
+    fn from(value: DerivationPath) -> Self {
+        Self {
+            path: value,
+            ..Default::default()
+        }
+    }
 }
 
 impl AccountInputBuilder {
-    pub fn new() {}
+    pub fn path(&mut self, path: DerivationPath) {
+        self.path = path;
+    }
+
+    pub fn blockchain(&mut self, blockchain: Blockchain) {
+        self.blockchain = blockchain;
+    }
+
+    pub fn network(&mut self, network: Network) {
+        self.network = network;
+    }
+
+    pub fn encrypted_seed(&mut self, encrypted_seed: &'static str) {
+        self.encrypted_seed = encrypted_seed;
+    }
+
+    fn build(&self, key: AESKey) -> AccountInputBuilderResult {
+        let path = &self.path;
+        let secp = secp256k1::Secp256k1::new();
+        let seed = decrypt(&key, self.encrypted_seed.as_bytes());
+        let bitcoin_network = self.blockchain.to_bitcoin_network();
+        let xprv = Xpriv::new_master(bitcoin_network, &seed);
+
+        if let Err(_) = xprv {
+            return Err(AccountError::Path);
+        }
+
+        let xprv = xprv.unwrap().derive_priv(&secp, path);
+
+        if let Err(_) = xprv {
+            return Err(AccountError::Derivation);
+        }
+
+        let xprv = xprv.unwrap();
+
+        let pk = PrivateKey::new(xprv.private_key, self.network.to_bitcoin_network_kind());
+        let c_pk = CompressedPublicKey::from_private_key(&secp, &pk)
+            .expect("Failed while attempting to create compressed pub key from slice.");
+        let address = match &self.blockchain {
+            Blockchain::Bitcoin => Address::p2wpkh(&c_pk, bitcoin_network),
+        };
+
+        let encrypted_path = &self.path.to_string();
+        let encrypted_path = encrypted_path.as_bytes();
+        let encrypted_path = encrypt(&key, encrypted_path).to_hex_string(bitcoin::hex::Case::Lower);
+
+        Ok(StoreAccountInput {
+            address: address.to_string(),
+            blockchain: self.blockchain,
+            encrypted_path,
+            network: Network::Mainnet,
+            wallet_id: self.wallet_id.clone(),
+            ..Default::default()
+        })
+    }
 }
