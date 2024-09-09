e66b5f8eab2f039e9ac5c474f5a54e399548bfe0
igdev <dorultanianos@gmail.com>
2024-09-09 16:48:37 +0300

    fix: tests

--- src-tauri/tests/main.rs
+++ src-tauri/tests/main.rs
@@ -10,10 +10,10 @@ async fn can_create_wallet() {
     let wallet = wallet.build();
     let name = wallet.name.clone();
     let vault = SqliteVault::new(Some("sqlite::memory:")).await;
+    vault.migrate().await;
 
     vault.insert_wallet(wallet).await.unwrap();
 
     let result_wallet = vault.get_wallet_by_name(&name).await.unwrap();
     assert_eq!(result_wallet.name, name);
 }
-src/vault/sqlite.rs
