5431a25d8958d9cb4fa57ab6468e665cdf400c3f
igdev <dorultanianos@gmail.com>
2024-09-10 12:15:15 +0300

    test(integration): add int test for creating a wallet

--- src-tauri/tests/main.rs
+++ src-tauri/tests/main.rs
@@ -1,17 +1,21 @@
 use dev_wallet::*;
 use tokio;
-use vault::{interface::VaultInterface, sqlite::SqliteVault, wallet::WalletInputBuilder};
+use vault::{
+    account::AccountInputBuilder, interface::VaultInterface, sqlite::SqliteVault,
+    wallet::WalletInputBuilder,
+};
 
 #[tokio::test]
 async fn can_create_wallet() {
     let mut wallet = WalletInputBuilder::new();
+
     wallet.name("name");
     wallet.password("password");
-    let wallet = wallet.build();
+
+    let wallet = wallet.build().unwrap();
     let name = wallet.name.clone();
     let vault = SqliteVault::new(Some("sqlite::memory:")).await;
     vault.migrate().await;
-
     vault.insert_wallet(wallet).await.unwrap();
 
     let result_wallet = vault.get_wallet_by_name(&name).await.unwrap();
