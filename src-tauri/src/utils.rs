b899634d798972cfcbe0a742b5c79b0b373dc973
igdev <dorultanianos@gmail.com>
2024-09-10 10:53:00 +0300

    refactor(AESUtil)!: add proper error handling \n BREAKING CHANGE: AES encrypt and decrypt returns a Result instead of bytes

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -5,7 +5,8 @@ use rand_core::{self, OsRng};
 
 use aes_gcm::aead::generic_array::GenericArray;
 use aes_gcm::aead::{Aead, KeyInit};
-use aes_gcm::{AeadCore, Aes256Gcm, Nonce}; // Or `Aes128Gcm`
+use aes_gcm::{AeadCore, Aes256Gcm, Nonce};
+use thiserror::Error; // Or `Aes128Gcm`
 
 pub fn generate_mnemonic() -> Result<Mnemonic, bip39::Error> {
     // 1. Generate entropy (for 24 mnemonic words)
@@ -20,6 +21,15 @@ pub fn generate_mnemonic() -> Result<Mnemonic, bip39::Error> {
     Mnemonic::from_entropy(&entropy)
 }
 
+#[derive(Error, Debug)]
+pub enum AESError {
+    #[error("Failed to encrypt: {0}")]
+    Encrypt(String),
+    #[error("Failed to decrypt: {0}")]
+    Decrypt(String),
+}
+
+pub type AESResult = Result<Vec<u8>, AESError>;
 pub type AESKey = [u8; 32];
 
 /// Encrypts the given plaintext using AES-GCM.
@@ -32,15 +42,22 @@ pub type AESKey = [u8; 32];
 /// # Returns
 ///
 /// An vector containing the nonce and the ciphertext as bytes.
-pub fn encrypt(key: &AESKey, data: &[u8]) -> Vec<u8> {
+pub fn encrypt(key: &AESKey, data: &[u8]) -> AESResult {
     // Create AES-GCM cipher
     let cipher = Aes256Gcm::new(GenericArray::from_slice(key));
 
+    println!("Decrypted with AESKey: {:#?}", &key);
     // Generate a random nonce
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-    let ciphertext = cipher.encrypt(&nonce, data).expect("encryption failure!");
+    let ciphertext = cipher.encrypt(&nonce, data);
 
-    [nonce.as_slice(), ciphertext.as_slice()].concat()
+    if let Err(err) = ciphertext {
+        return Err(AESError::Encrypt(err.to_string()));
+    }
+
+    let ciphertext = ciphertext.unwrap();
+
+    Ok([nonce.as_slice(), ciphertext.as_slice()].concat())
 }
 
 /// Decrypts the given ciphertext using AES-GCM.
@@ -53,17 +70,20 @@ pub fn encrypt(key: &AESKey, data: &[u8]) -> Vec<u8> {
 /// # Returns
 ///
 /// The decrypted plaintext.
-pub fn decrypt(key: &AESKey, data: &[u8]) -> Vec<u8> {
+pub fn decrypt(key: &AESKey, data: &[u8]) -> AESResult {
     // Create AES-GCM cipher
     let cipher = Aes256Gcm::new(GenericArray::from_slice(key));
 
+    println!("Decrypted with AESKey: {:#?}", key);
     // Convert nonce slice to Nonce type
     let (nonce, ciphertext) = data.split_at(12); // 96-bits; unique per message
-    let decrypted = cipher
-        .decrypt(Nonce::from_slice(nonce), ciphertext)
-        .expect("decryption failure!");
+    let decrypted = cipher.decrypt(Nonce::from_slice(nonce), ciphertext);
 
-    decrypted
+    if let Err(err) = decrypted {
+        return Err(AESError::Decrypt(err.to_string()));
+    }
+
+    Ok(decrypted.unwrap())
 }
 
 #[cfg(test)]
