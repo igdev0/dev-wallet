1d7cf1a4d2bd2d0fb4724aeb1d9ea35e9b09ed5b
igdev <dorultanianos@gmail.com>
2024-09-10 12:15:32 +0300

    test(integration): add int test for creating an account

--- src-tauri/tests/main.rs
+++ src-tauri/tests/main.rs
@@ -21,3 +21,31 @@ async fn can_create_wallet() {
     let result_wallet = vault.get_wallet_by_name(&name).await.unwrap();
     assert_eq!(result_wallet.name, name);
 }
+
+#[tokio::test]
+async fn can_create_account() {
+    let name = "main";
+    let password = "password123";
+    let mut wallet = WalletInputBuilder::new();
+
+    wallet.name(&name);
+    wallet.password(&password);
+
+    let wallet = wallet.build().unwrap();
+    let name = wallet.name.clone();
+    let vault = SqliteVault::new(Some("sqlite::memory:")).await;
+    vault.migrate().await;
+    vault.insert_wallet(wallet).await.unwrap();
+
+    let result_wallet = vault.get_wallet_by_name(&name).await.unwrap();
+
+    let key = result_wallet.authenticate(password).unwrap();
+    let account = AccountInputBuilder::from(result_wallet);
+
+    let account = account.build(key).unwrap();
+    let account = vault.insert_account(account).await.unwrap();
+
+    let account_retrieved = vault.get_account_by_id(&account.id).await.unwrap();
+
+    assert_eq!(account_retrieved.id, account.id);
+}
