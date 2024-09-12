e0f028dd606de7f5e60fc7cdb01a8fae7eec5fc5
igdev <dorultanianos@gmail.com>
2024-09-12 11:23:38 +0300

    refactor: screen component layout

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -1,12 +1,18 @@
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren {}
 
 export default function Screen({ children }: Props) {
   return (
-    <Box height="100vh" width="100%" overflow="auto" p={3}>
+    <Flex
+      height="100vh"
+      width="100%"
+      overflow="auto"
+      p={3}
+      flexDirection="column"
+    >
       {children}
-    </Box>
+    </Flex>
   );
 }
