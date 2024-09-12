51f5b7c54c72f5f6efb6ecf79020388c08a30a21
igdev <dorultanianos@gmail.com>
2024-09-12 11:26:29 +0300

    feat(screen): init 🎉 the new accounts screen

--- src/screens/accounts.tsx
+++ src/screens/accounts.tsx
@@ -1,20 +1,29 @@
 import Screen from "../components/screen.tsx";
-import { Heading } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import { Heading, Text } from "@chakra-ui/react";
 import Error from "../components/error.tsx";
+import useListAccounts from "../hooks/use-list-accounts.ts";
+import Loading from "../components/loading.tsx";
 
 export default function AccountsScreen() {
-  const params = useParams<{ wallet_id: string }>();
-
-  if (!params.wallet_id) {
+  let accountsRes = useListAccounts();
+  if (accountsRes.isError) {
     return (
       <Error error="This is not what you're looking for. Please login to list your accounts." />
     );
+  }
+  if (accountsRes.isLoading) {
+    return <Loading />;
   }
 
   return (
     <Screen>
       <Heading>Accounts screen</Heading>
+      {accountsRes.data?.map((account, index) => (
+        <div key={index}>
+          <Text>{account.address}</Text>
+          <Text>{account.network}</Text>
+        </div>
+      ))}
     </Screen>
   );
 }
