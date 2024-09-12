d8b214f6b21635007ea8021273dc852298f3f27b
igdev <dorultanianos@gmail.com>
2024-09-12 11:24:47 +0300

    refactor: update loading screen UI

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,16 +1,20 @@
-import { Box, Spinner } from "@chakra-ui/react";
+import { Flex, Spinner } from "@chakra-ui/react";
+import Screen from "./screen.tsx";
+import Navbar from "./navbar.tsx";
 
 export default function Loading() {
   return (
-    <Box
-      bg="teal.100"
-      w="100%"
-      h="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Spinner />
-    </Box>
+    <Screen>
+      <Navbar />
+      <Flex
+        height="100%"
+        flex={1}
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Spinner />
+      </Flex>
+    </Screen>
   );
 }
