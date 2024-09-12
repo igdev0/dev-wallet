b86ab40a25cc581ed7b1ec73f5f748953ed84c79
igdev <dorultanianos@gmail.com>
2024-09-12 11:24:13 +0300

    refactor: error screen UI

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,18 +1,26 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
+import Screen from "./screen.tsx";
+import Navbar from "./navbar.tsx";
 
 export default function Error(props: { error: string }) {
   return (
-    <Box
-      bg="teal.100"
-      w="100%"
-      h="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Heading>App error</Heading>
-      <Text>{props.error}</Text>
-    </Box>
+    <Screen>
+      <Navbar />
+      <Box
+        height="100%"
+        flex={1}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Heading _light={{ color: "black" }} textAlign="center">
+          App error
+        </Heading>
+        <Text _light={{ color: "gray.900" }} textAlign="center">
+          {props.error}
+        </Text>
+      </Box>
+    </Screen>
   );
 }
