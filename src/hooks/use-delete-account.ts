0e546f4fe2a6b2b783c4cb2eca738fe41fa2dda2
igdev <dorultanianos@gmail.com>
2024-09-13 12:44:07 +0300

    feat: delete account hook

---
+++ src/hooks/use-delete-account.ts
@@ -0,0 +1,30 @@
+import { useMutation } from "react-query";
+import useListAccounts from "./use-list-accounts.ts";
+import { invoke } from "@tauri-apps/api";
+
+type DeleteAccountInput = {
+  walletID: string;
+  accountID: string;
+};
+
+type DeleteAccountResponse = {
+  success: boolean;
+};
+
+export default function useDeleteAccount() {
+  const listQuery = useListAccounts(false);
+  return useMutation<DeleteAccountResponse, string, DeleteAccountInput>(
+    "delete-wallet",
+    async (input) => {
+      return await invoke("delete_account", {
+        walletId: input.walletID,
+        accountId: input.accountID,
+      });
+    },
+    {
+      async onSuccess() {
+        await listQuery.refetch();
+      },
+    },
+  );
+}
