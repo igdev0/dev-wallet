f45aa4b2d43fd1a3195d968f64dcdfd9b6f057b1
igdev <dorultanianos@gmail.com>
2024-09-14 12:29:16 +0300

    feat: remove ref util

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,7 @@ const regex = /^((\d+'?)(\/\d+'?)*)$/;
 export function isValidDerivationPath(path: string): boolean {
   return regex.test(path);
 }
+
+export function removeRef(data: object) {
+  return JSON.parse(JSON.stringify(data));
+}
