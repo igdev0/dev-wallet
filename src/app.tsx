e6e6bdcaff4d4925d82fbc830275f0f6ae8fe0bf
igdev <dorultanianos@gmail.com>
2024-09-14 10:59:52 +0300

    refactor: impl the ErrorBoundary & fallbackError for accounts screen

--- src/app.tsx
+++ src/app.tsx
@@ -3,16 +3,31 @@ import CreateWalletScreen from "./screens/create-wallet.tsx";
 import { useAppState } from "./state.ts";
 import AuthenticateScreen from "./screens/authenticate.tsx";
 import AccountsScreen from "./screens/accounts.tsx";
+import { Suspense } from "react";
+import Loading from "./components/loading.tsx";
+import { QueryErrorResetBoundary } from "react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import fallbackError from "./components/fallback-error.tsx";
 
 function App() {
   const appState = useAppState();
-
   return (
     <BrowserRouter>
       <Routes>
         <Route index={true} path="/" element={<CreateWalletScreen />} />
         <Route path="/authenticate" element={<AuthenticateScreen />} />
-        <Route path="/accounts/:wallet_id" element={<AccountsScreen />} />
+        <Route
+          path="/accounts/:wallet_id"
+          element={
+            <QueryErrorResetBoundary>
+              <ErrorBoundary fallbackRender={fallbackError}>
+                <Suspense fallback={<Loading />}>
+                  <AccountsScreen />
+                </Suspense>
+              </ErrorBoundary>
+            </QueryErrorResetBoundary>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
