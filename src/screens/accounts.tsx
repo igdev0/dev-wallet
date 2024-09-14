b81b8845ac3e8b316b0f4cf379d58b2f0243035a
igdev <dorultanianos@gmail.com>
2024-09-14 11:02:04 +0300

    refactor: remove the unnecessary error & loading renderer

--- src/screens/accounts.tsx
+++ src/screens/accounts.tsx
@@ -1,19 +1,9 @@
 import Screen from "../components/screen.tsx";
 import { Heading, Text } from "@chakra-ui/react";
-import Error from "../components/error.tsx";
 import useListAccounts from "../hooks/use-list-accounts.ts";
-import Loading from "../components/loading.tsx";
 
 export default function AccountsScreen() {
   let accountsRes = useListAccounts();
-  if (accountsRes.isError) {
-    return (
-      <Error error="This is not what you're looking for. Please login to list your accounts." />
-    );
-  }
-  if (accountsRes.isLoading) {
-    return <Loading />;
-  }
 
   return (
     <Screen>
