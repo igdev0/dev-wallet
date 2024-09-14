322c71285203556ada4d7110a679538ea974eb7e
igdev <dorultanianos@gmail.com>
2024-09-14 12:29:37 +0300

    refactor: account creation input

--- src/hooks/use-create-account.ts
+++ src/hooks/use-create-account.ts
@@ -1,16 +1,11 @@
 import { useMutation } from "react-query";
 import { invoke } from "@tauri-apps/api";
 import { useParams } from "react-router-dom";
-import useListAccounts, {
-  Account,
-  AccountBlockchain,
-  AccountNetwork,
-} from "./use-list-accounts.ts";
+import useListAccounts, { Account } from "./use-list-accounts.ts";
 
 type CreateAccountInput = {
   path: string;
-  network: AccountNetwork;
-  blockchain: AccountBlockchain;
+  password: string;
 };
 
 export default function useCreateAccount() {
@@ -18,11 +13,11 @@ export default function useCreateAccount() {
   const listQuery = useListAccounts(false);
   return useMutation<Account, string, CreateAccountInput>(
     "create-account",
-    async ({ path, network, blockchain }) => {
+    async ({ path, password }) => {
       return await invoke("create_account", {
         walletId: wallet_id,
         path,
-        blockchain,
+        password,
       });
     },
     {
