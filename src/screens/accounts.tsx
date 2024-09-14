e296ea572b55e6dd5b3231935874ccbc4c70b335
igdev <dorultanianos@gmail.com>
2024-09-14 11:25:53 +0300

    refactor(accounts): implement basic style for accounts list

--- src/screens/accounts.tsx
+++ src/screens/accounts.tsx
@@ -1,19 +1,48 @@
 import Screen from "../components/screen.tsx";
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Badge,
+  IconButton,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import useListAccounts from "../hooks/use-list-accounts.ts";
+import Navbar from "../components/navbar.tsx";
+import { Copy01, Trash01 } from "@untitled-ui/icons-react";
 
 export default function AccountsScreen() {
-  let accountsRes = useListAccounts();
+  const accountsRes = useListAccounts();
 
   return (
     <Screen>
-      <Heading>Accounts screen</Heading>
-      {accountsRes.data?.map((account, index) => (
-        <div key={index}>
-          <Text>{account.address}</Text>
-          <Text>{account.network}</Text>
-        </div>
-      ))}
+      <Navbar />
+      <Table overflowY="auto">
+        <Thead>
+          <Th>Address</Th>
+          <Th>Blockchain</Th>
+          <Th>Network</Th>
+        </Thead>
+        <Tbody>
+          {accountsRes.data?.map((item, index) => (
+            <Tr key={index}>
+              <Td>{item.address}</Td>
+              <Td>
+                <Badge colorScheme="teal">{item.blockchain}</Badge>
+              </Td>
+              <Td>
+                <Badge>{item.network}</Badge>
+              </Td>
+              <Td gap={2} display="flex">
+                <IconButton aria-label="copy" icon={<Copy01 />} />
+                <IconButton aria-label="delete" icon={<Trash01 />} />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Screen>
   );
 }
